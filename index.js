b40b6ab4c044413eeb5d60295a9c9cbcff8c9e41
Mindia Shantadze <shantadze200202@gmail.com>
2021-12-27 18:11:24 +0400

    finished building christmas tree

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
-const christmasTree = document.querySelector('.christmas-tree');
+const christmasTreePage = document.querySelector('.christmas-tree-page');
 
-let newArr = [];
+function drawChristmasTree(size) {
+    // getting table from DOM to build christmas tree on it
+    const christmasTree = document.querySelector('.christmas-tree');
 
-for (let i=0;i<10;i++) {
-    let row = [0];
-    for (let j=0;j<i;j++) {
-        row.push(j+1);
-    }
-    newArr.push(row);
-}
+    // defining new array to hold tree branches
+    let newArr = [];
 
-for (let i=0;i<newArr.length;i++) {
-
-    const row = document.createElement('tr');
-    let christmasTreeStr = '';
-    
-    for (let j=0;j<newArr[i].length;j++) {
-        if (i ===0 && j===0) {
-            christmasTreeStr+=`<td class='christmas-tree-branch christmas-tree-star-branch'>${newArr[i][j]}</td>`
+    // filling array with random numbers
+    for (let i=0;i<size;i++) {
+        let row = [0];
+        for (let j=0;j<i;j++) { 
+            row[0] = Math.ceil(Math.random() * 9);
+            row.push(Math.ceil(Math.random() * 9))
         }
-        else {
-            christmasTreeStr+=`<td class='christmas-tree-branch'>${newArr[i][j]}</td>`;
-        }
+        newArr.push(row);
     }
 
-    row.innerHTML = christmasTreeStr;
-    christmasTree.appendChild(row);
+    // printing christmas tree on the page
+    for (let i=0;i<newArr.length;i++) {
+
+        const row = document.createElement('tr');
+
+        for (let j=0;j<newArr[i].length;j++) {
+            const treeBranch = document.createElement('td');
+            treeBranch.className = (newArr[i][j] === 0) ? 'christmas-tree-star-branch' : 'christmas-tree-branch';
+
+            if (newArr[i][j] %2 === 0) {
+                setInterval(() => {
+                    treeBranch.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+                }, 1000);
+            }
+            row.appendChild(treeBranch);
+        }
+
+        christmasTree.appendChild(row);
+    }
 }
+
+drawChristmasTree(40);
